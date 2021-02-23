6decaca2d7cf6a8d41b6621a8b18733f43fb3ba9
Gilang <ondozwahyudi@gmail.com>
2021-02-23 08:48:07 +0700

    Membuat Style

--- App.js
+++ App.js
@@ -1,11 +1,94 @@
 import React, {Component} from 'react';
-import {Image, Text, TextInput, View} from 'react-native';
+import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
+import Image_1 from './Image-1.png';
 
 // export default function App() {
 //   return <Text>Hallo Word</Text>;
 // }
 
 const App = () => {
+  return <StyleReactNativeComponent />;
+};
+
+const StyleReactNativeComponent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Style Component</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: '#0abde3',
+          borderWidth: 2,
+          borderColor: '#5f27cd',
+          margin: 20,
+        }}
+      />
+
+      <View
+        style={{
+          padding: 12,
+          backgroundColor: '#F2F2F2',
+          width: 212,
+          borderRadius: 8,
+        }}>
+        <Image
+          source={Image_1}
+          style={{width: 188, height: 107, borderRadius: 8}}
+        />
+        <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>
+          Tanjung Ringgit
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: 'bold',
+            color: '#F2994A',
+            marginTop: 12,
+          }}>
+          Rp.10.000.000
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: 'bold',
+            marginTop: 12,
+          }}>
+          Lombok Timur
+        </Text>
+
+        <View
+          style={{
+            backgroundColor: '#6FCF97',
+            paddingVertical: 6,
+            borderRadius: 25,
+            marginTop: 20,
+          }}>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontWeight: '600',
+              fontSize: 14,
+              color: 'white',
+            }}>
+            Beli
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'green',
+    margin: 20,
+  },
+});
+
+const SampleComponent = () => {
   return (
     <View>
       <Pictue />
@@ -50,7 +133,7 @@ class BoxGreen extends Component {
   render() {
     return (
       <Text style={{color: '#3498db', fontSize: 16, fontWeight: 'bold'}}>
-        ini Commponent yang berasal dari class
+        ini Commponent yang berasal dari class BoxGreen
       </Text>
     );
   }
